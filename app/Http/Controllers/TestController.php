df30a083d052915f3586cdfb989fb0f874e70c85
Alessandro Ferraro <ale_ferraro@libero.it>
2021-02-08 18:30:16 +0100

    Crea 2 viste per guest e logged user
    Guest:
    - la pagina /free-zone/hello mostra il messaggio: 'ciao utente'
    
    Logged:
    - la pagina /restricted-zone/hello mostra il messaggio: 'ciao $nome_utente'
    
    entrambe le pagine hanno una funzione di redirect in base a se l'utente è loggato o meno

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+class TestController extends Controller
+{
+    public function guest()
+    {   
+        if(Auth::check()){
+            return redirect('restricted-zone/hello');
+        } else{
+            return view('hello');
+        }
+       
+    }
+
+    public function logged()
+    {   
+        if(Auth::check()){
+            $user = Auth::user();
+
+            return view('hello', compact('user'));
+        } else {
+            return redirect('free-zone/hello');
+        }
+
+    }
+}
